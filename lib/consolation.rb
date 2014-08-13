08a44948b81d5f3747daf3abd86dea726a849d43
Jake Cataford <jakecataford@gmail.com>
2014-08-13 16:42:58 -0400

    Simplifies everything. No more weirdness
    Start of a heavy refactor to move parsing logic to the client and stop
    taking so long to render long output chunks.
    
    Rethinking the refreshing as well.

--- lib/consolation.rb
+++ lib/consolation.rb
@@ -1,187 +1,23 @@
-require 'log_source'
-require 'adapters/adapter'
-Dir.glob(File.expand_path("../adapters/**/*.rb", __FILE__)).each do |file|
-  require file
-end
-
+require 'erb'
+require 'active_support'
+require 'active_support/core_ext/string/output_safety'
 
 module Consolation
   module Rails
     class Engine < ::Rails::Engine
-      require 'spinjs-rails'
-      require 'zeroclipboard-rails'
     end
   end
 
-  def to_console_line_html string
-    output = ""
-    in_tag_with_class output, "tr", "line" do
-      put_line_number output, ""
-      escape_string_for_ansi string
-      put_console_line output, string, ""
-    end
-    return output
-  end
-
-  def console log_output, options={}
+  def console(log_output, options={})
     defaults = {
       no_auto_lines: false,
-      truncate_to: 150,
-      height: 0,
+      height: 0
     }
 
     options = defaults.merge(options)
-    puts options[:live]
-    live_source = options[:live]
-    auto_line_numbers = true
-    auto_line_numbers = false if log_output =~ /\A\d+\s/
-    auto_line_counter = 1
-    output = ""
-    inside_console_tags output, options[:height], live_source do
-      put_copy_prompt output
-      in_tag_with_class output, "tr", "line" do
-        put_padding_line_number output
-        put_padding_line output
-      end
-
-      total_lines = log_output.lines.count
-      if total_lines > options[:truncate_to]
-        log_output = log_output.split("\n").last(options[:truncate_to]).join("\n")
-        auto_line_counter = total_lines - options[:truncate_to]
-        put_expandable_line output, total_lines - options[:truncate_to]
-      end
-
-      log_output.each_line do |line|
-        in_tag_with_class output, "tr", "line" do
-          line_number = auto_line_counter if auto_line_numbers
-          line_number ||= /\A\d+\s/.match(line).first
-          put_line_number output, "#{line_number}"
-          put_console_line output, escape_string_for_ansi(line), line_number
-          auto_line_counter += 1 if auto_line_numbers
-        end
-      end
-
-      unless live_source.nil?
-        in_tag_with_class output, "tr", "consolation-ajax-placeholder" do
-          put_ajax_line output
-        end
-      end
-
-      in_tag_with_class output, "tr", "line" do
-        put_padding_line_number output
-        put_padding_line output
-      end
-
-    end
-    return output.html_safe
-  end
-
-  private
-  NON_BREAKING_SPACE = "&nbsp;"
-
-  ANSI_COLOR_MAP = { 1 => :nothing,
-                     2 => :nothing,
-                     4 => :nothing,
-                     5 => :nothing,
-                     7 => :nothing,
-                     30 => :black,
-                     31 => :red,
-                     32 => :green,
-                     33 => :yellow,
-                     34 => :blue,
-                     35 => :magenta,
-                     36 => :cyan,
-                     37 => :white,
-                     39 => :bold,
-                     40 => :nothing,
-                     41 => :nothing,
-                     43 => :nothing,
-                     44 => :nothing,
-                     45 => :nothing,
-                     46 => :nothing,
-                     47 => :nothing,
-  }
-
-  URL_REGEX = /https?:\/\/[A-Za-z0-9\.\/]+/
-
-  def inside_console_tags output, max_height, live_url
-    puts live_url
-    output << "<table class='consolation-console' "
-    output << "style='max-height: #{max_height}px' " if max_height > 0
-    output << "data-live-source='#{live_url}' " unless live_url.nil?
-    output << ">"
-    yield
-    output << "</table>"
-  end
-
-  def in_tag_with_class output, tag, dom_class, data_attributes={}
-    output << "<#{tag} class='#{dom_class}' "
-    data_attributes.each do |key, value|
-      output << "data-#{key}='#{value}' "
-    end
-    output << ">"
-    yield
-    output << "</#{tag}>"
-  end
-
-  def put_line_number output, number
-    in_tag_with_class output, "td", "consolation-line-number" do
-      output << number
-    end
-  end
-
-  def put_console_line output, string, line_number
-    in_tag_with_class output, "td", "consolation-line", line: line_number do
-      output << string
-    end
-  end
-
-  def put_padding_line_number output
-    in_tag_with_class output, "td", "consolation-padding-line-number" do
-      output << NON_BREAKING_SPACE
-    end
-  end
-
-  def put_padding_line output
-    in_tag_with_class output, "td", "consolation-padding-line" do
-      output << NON_BREAKING_SPACE
-    end
-  end
-
-  def put_expandable_line output, num_lines_omitted
-    in_tag_with_class output, "td", "consolation-truncation-line-number" do
-      output << "..."
-    end
-    in_tag_with_class output, "td", "consolation-truncation" do
-      output << "Log too long, #{num_lines_omitted} lines omitted."
-    end
-  end
-
-  def put_ajax_line output
-    put_line_number output, "..."
-    put_console_line output, "", "placeholder"
-  end
-
-  def escape_string_for_ansi string
-    ANSI_COLOR_MAP.each do |key, value|
-      if value != :nothing
-        string.gsub!(/\[#{key}m/,"<span class=\"consolation-ansi-#{value}\">")
-        string.gsub!(/\[#{key};1m/,"<span class=\"consolation-ansi-#{value}\">")
-      else
-        string.gsub!(/\[#{key}m/,"<span>")
-      end
-    end
-    string.gsub!(URL_REGEX) { |s| "<a href='#{s}'>#{s}</a>" }
-    string.gsub!(/\[0m/,'</span>')
-    return string
-  end
-
-  def put_copy_prompt output
-    in_tag_with_class output, "div", "consolation-copy-prompt" do
-      output << image_tag("consolation_copy_prompt_ic.png")
-      in_tag_with_class output, "span", "consolation-copy-prompt-long" do
-        output << "Stop dragging to copy to clipboard."
-      end
-    end
+    template_path   = File.expand_path("../views/console.html.erb", __FILE__)
+    template_string = File.read(template_path)
+    rendered_text   = "<pre class='consolation'>" + log_output + "</pre>"
+    rendered_text.html_safe
   end
 end
--- vendor/assets/javascripts/consolation.js.coffee
+++ vendor/assets/javascripts/consolation.js.coffee
@@ -1,102 +1,16 @@
-#=require jquery
-#=require jquery.easing
-#=require zeroclipboard
-#=require jquery.spin
-
-unless $? || jQuery?
-  throw "Consolation Error: jQuery is a dependency of consolation."
-
-$(document).ready ->
-  consolation_controller = new Consolation()
-
 class Console
-  is_in_range: (number,a,b) =>
-    min = Math.min.apply(Math, [a,b])
-    max = Math.max.apply(Math, [a,b])
-    return number >= min && number <= max
 
-  drag_start: (event) =>
-    @copy_prompt.css({left: event.clientX, top: event.clientY})
-    @copy_prompt.addClass('ready')
-    @copy_prompt.animate({opacity:0.7}, 250)
-    @line_drag_start = $(event.delegateTarget).data('line')
-    @current_timeout_id = setTimeout(@show_extended_prompt, 3000)
+  @sub_ansi_colors: () ->
+  
+  constructor: (element) ->
+    console.log(element.innerHtml)
 
-
-  show_extended_prompt: () =>
-    @copy_prompt.css({width: 'auto'})
-    width = @copy_prompt.width()
-    @copy_prompt.css({width: @starting_width})
-    @copy_prompt.animate({width: width}, 500)
-
-  drag_move: (event) =>
-    @copy_prompt.css({left: event.clientX + 16, top: event.clientY - 16})
-
-  drag_over: (event) =>
-    if @line_drag_start > -1
-      @line_drag_to = $(event.delegateTarget).data('line')
-      @element.find('.consolation-line').each (index, elem) =>
-        if @is_in_range($(elem).data('line'), @line_drag_start, @line_drag_to)
-          $(elem).addClass('selected')
-        else
-          $(elem).removeClass('selected')
-
-  drag_end: (event) =>
-    @copy_prompt.fadeTo(1.0, 30)
-    clearTimeout(@current_timeout_id)
-    @copy_prompt.animate { top: @copy_prompt.offset().top - $(window).scrollTop() - 35, opacity: 0, width: @starting_width}, 500, =>
-      @copy_prompt.removeClass('ready')
-    @line_drag_to = $(event.delegateTarget).data('line')
-    @element.find('.consolation-line').removeClass("selected")
-    console.log("Selected #{Math.abs(@line_drag_start - @line_drag_to) + 1} lines from #{@line_drag_start} to #{@line_drag_to}")
-
-    text_to_copy = ""
-    @element.find('.consolation-line').each (index, elem) =>
-        if @is_in_range($(elem).data('line'), @line_drag_start, @line_drag_to)
-          text_to_copy += $(elem).text()
-    console.log @clipboard.setText text_to_copy
-    @line_drag_start = -1
-    @line_drag_end = -1
-    @element.find('.consolation-line').removeClass("selected")
-
-  drag_cancel: (event) =>
-    clearTimeout(@current_timeout_id)
-    @copy_prompt.animate { top: @copy_prompt.offset().top - $(window).scrollTop() + 35, opacity: 0, width: @starting_width}, 250, =>
-      @copy_prompt.removeClass('ready')
-
-    @line_drag_start = -1
-    @line_drag_end = -1
-    @element.find('.consolation-line').removeClass("selected")
-
-  bind_to_events: () =>
-    @element.find('.consolation-line').mousedown(@drag_start)
-    @element.find('.consolation-line').mouseup(@drag_end)
-    @element.find('.consolation-line').mouseover(@drag_over)
-    @element.find('.consolation-line').mousemove(@drag_move)
-    $(document).mouseup(@drag_cancel)
-
-  init: () ->
-    @bind_to_events()
-
-  live_message_recieved: (event) =>
-    if event.data.match(/^EOF\s/)
-      event.target.close()
-    else
-      @element.find('.consolation-ajax-placeholder').before(event.data)
-
-  constructor: (@element, @line_drag_start = -1, @line_drag_to = -1) ->
-    @copy_prompt = $(@element.parent().find('.consolation-copy-prompt')[0])
-    @current_timeout_id = 0
-    @starting_width = @copy_prompt.width()
-    @clipboard = new ZeroClipboard();
-    if @element.data('live-source')
-      @element.find('.placeholder').spin()
-      @event_source = new EventSource(@element.data('live-source'))
-      @event_source.onmessage = @live_message_recieved
 
 class Consolation
-
   constructor: () ->
-    $('.consolation-console').each ->
-      console = new Console($(this))
-      console.init()
+    consoles = document.querySelector('.consolation-console') 
+    new Console(console) for console in consoles
+
+document.addEventListener "DOMContentLoaded", ->
+  consolation_controller = new Consolation()
+
