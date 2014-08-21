53059cc7dc52623744a2c61389c776203e82d146
Justin Mutter <mutter.justin@gmail.com>
2014-08-21 11:11:04 -0400

    Only render json for /tail

--- app/controllers/concerns/loggable_controller.rb
+++ app/controllers/concerns/loggable_controller.rb
@@ -5,11 +5,11 @@ module LoggableController
 
     def tail
       chunks = @loggable.log_chunks.tail(params[:last_id])
-      respond_with({
-          url: @loggable.log_tail_url,
+      render json: {
+          tail_url: @loggable.tail_url,
           log_chunks: chunks,
           :"#{@loggable.class.name.parameterize}" => @loggable
-        })
+        }
     end
 
   end
